8014bfc2e14df8c83adf15387589bf8337602b45
Francesco Martino <fra.martino7@stud.uniroma3.it>
2022-06-02 19:45:51 +0200

    first commit: - initial SpringBoot project structure

---
+++ src/main/java/it/uniroma3/siw/catering/Siw2022DocenteApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.catering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Siw2022DocenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Siw2022DocenteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/uniroma3/siw/catering/Siw2022DocenteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.catering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Siw2022DocenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
